49d603c1c4b35c34af380f07cee8f76a4fee4f35
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2024-04-07 03:52:05 +0900

    0. gradle updata / script 수정
    - gradle update gradle wrapper 8.5 업데이트 (6.7.1 이상 필요)
    - gradle kts로 변환 (각종 플러그인 추가)

---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    val springBootVersion = "2.7.18"
+    val kotlinVersion = "1.9.20"
+    java
+    id("org.springframework.boot") version springBootVersion
+    id("io.spring.dependency-management") version "1.1.4"
+    // kotlin jvm 플러그인
+    kotlin("jvm") version kotlinVersion
+    // kotlin spring 호환성(open class) 플러그인
+    kotlin("plugin.spring") version kotlinVersion
+    // Kotlin Annotation Processing Tool
+    kotlin("kapt") version kotlinVersion
+    // kotlin jpa 호환성(entity class에 매개변수 없는 기본 생성자 생성)
+    kotlin("plugin.jpa") version kotlinVersion
+    // kotlin에서 lombok 사용이 가능해지게 만들어주는 플러그인
+    kotlin("plugin.lombok") version kotlinVersion
+    id("io.freefair.lombok") version "8.1.0"
+}
+group = "com.makers"
+version = "0.0.1-SNAPSHOT"
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+repositories {
+    mavenCentral()
+}
+dependencies {
+// querydsl
+    val querydslVersion = "5.0.0"
+    implementation("com.querydsl:querydsl-jpa:$querydslVersion")
+    kapt("com.querydsl:querydsl-apt:$querydslVersion:jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    runtimeOnly("com.h2database:h2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+kapt {
+    keepJavacAnnotationProcessors = true
+}
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+    compileKotlin {
+        kotlinOptions {
+            freeCompilerArgs += "-Xjsr305=strict"
+            jvmTarget = "11"
+        }
+    }
+    compileTestKotlin {
+        kotlinOptions {
+            jvmTarget = "11"
+        }
+    }
+}
