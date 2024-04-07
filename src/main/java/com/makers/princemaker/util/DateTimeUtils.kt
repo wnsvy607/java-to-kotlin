ffd697ddce52a5ae5105be94d6e91ba432ad4358
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2024-04-07 21:16:26 +0900

    1. util class 변환
    - object 키워드 활용 싱글톤으로 생성
    - package level function(top level function)으로 함수만 생성

--- src/main/java/com/makers/princemaker/util/DateTimeUtils.kt
+++ src/main/java/com/makers/princemaker/util/DateTimeUtils.kt
@@ -1,15 +1,33 @@
-package com.makers.princemaker.util;
+package com.makers.princemaker.util
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 
-import lombok.experimental.UtilityClass;
+/**
+ * object 키워드를 활용해 싱글톤으로 생성하는 예제
+ * static 으로 사용하고자 하는 메서드에 @JvmStatic 을 붙여준다.
+ */
+//object DateTimeUtils {
+//    @JvmStatic
+//    fun getLocalDateTimeString(localDateTime: LocalDateTime): String {
+//        return localDateTime.format(
+//            DateTimeFormatter
+//                .ofPattern("yyyy-MM-dd 탄생")
+//        )
+//    }
+//}
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-@UtilityClass
-public class DateTimeUtils {
-    public static String getLocalDateTimeString(LocalDateTime localDateTime) {
-        return localDateTime.format(DateTimeFormatter
-                .ofPattern("yyyy-MM-dd 탄생"));
-    }
+/**
+ * top level function 을 활용해 생성하는 예제
+ */
+fun getLocalDateTimeString(localDateTime: LocalDateTime): String {
+    return localDateTime.format(
+        DateTimeFormatter
+            .ofPattern("yyyy-MM-dd 탄생")
+    )
 }
+
+/**
+ * 두 방법 중 편한 것을 쓰면 된다.
+ */
