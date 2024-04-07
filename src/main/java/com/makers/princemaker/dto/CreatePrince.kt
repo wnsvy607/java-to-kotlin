85b14dec775a34f19489e56488bb37f205bf8b24
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2024-04-07 21:50:15 +0900

    2. DTO 변환

--- src/main/java/com/makers/princemaker/dto/CreatePrince.kt
+++ src/main/java/com/makers/princemaker/dto/CreatePrince.kt
@@ -1,70 +1,59 @@
-package com.makers.princemaker.dto;
+package com.makers.princemaker.dto
 
-import com.makers.princemaker.entity.Prince;
-import com.makers.princemaker.type.PrinceLevel;
-import com.makers.princemaker.type.SkillType;
-import lombok.*;
-
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import com.makers.princemaker.entity.Prince
+import com.makers.princemaker.type.PrinceLevel
+import com.makers.princemaker.type.SkillType
+import lombok.*
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
 
 /**
- * @author Snow
+ * 1. nullable 에 대해 체크 주의가 필요하다.
+ * - DB, 외부 API 요청/응답 등 외부 환경과 연관된 DTO 는 주의하자.
+ *
+ * 2. Lombok으로 생성되었던 getter/setter/생성자/toString/equals 등은 상황에 맞춰 수정 필요
+ * - getter/setter: 코틀린은 기본적으로 getter/setter를 제공핳는 프로퍼티로 클래스가 작성됨
+ * - 생성자: intelliJ 코틀린 변환기로 변환 후 코틀린 주 생성자 스타일로 바꿔주는 것 추천
+ * - toString/equals: data class로 변환하는 것이 좋다.
  */
-public class CreatePrince {
+class CreatePrince {
 
-    @Getter
-    @Setter
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Builder
-    public static class Request {
+    data class Request(
         @NotNull
-        private PrinceLevel princeLevel;
-
+        val princeLevel: PrinceLevel? = null,
         @NotNull
-        private SkillType skillType;
+        val skillType: SkillType? = null,
+        @NotNull @Min(0)
+        val experienceYears: Int? = null,
+        @NotNull @Size(min = 3, max = 50, message = "invalid princeId")
+        val princeId: String? = null,
+        @NotNull @Size(min = 2, max = 50, message = "invalid name")
+        val name: String? = null,
+        @NotNull @Min(18)
+        val age: Int? = null
+    )
 
-        @NotNull
-        @Min(0)
-        private Integer experienceYears;
-
-        @NotNull
-        @Size(min = 3, max = 50, message = "invalid princeId")
-        private String princeId;
-
-        @NotNull
-        @Size(min = 2, max = 50, message = "invalid name")
-        private String name;
-
-        @NotNull
-        @Min(18)
-        private Integer age;
-    }
-
-    @Getter
-    @Setter
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Builder
-    public static class Response {
-        private PrinceLevel princeLevel;
-        private SkillType skillType;
-        private Integer experienceYears;
-        private String princeId;
-        private String name;
-        private Integer age;
-
-        public static Response fromEntity(Prince prince) {
-            return Response.builder()
-                    .princeLevel(prince.getPrinceLevel())
-                    .skillType(prince.getSkillType())
-                    .experienceYears(prince.getExperienceYears())
-                    .princeId(prince.getPrinceId())
-                    .name(prince.getName())
-                    .age(prince.getAge())
-                    .build();
+    data class Response(
+        val princeLevel: PrinceLevel? = null,
+        val skillType: SkillType? = null,
+        val experienceYears: Int? = null,
+        val princeId: String? = null,
+        val name: String? = null,
+        val age: Int? = null
+    ) {
+        companion object {
+            @JvmStatic
+            fun fromEntity(prince: Prince): Response {
+                return Response(
+                    princeLevel = prince.princeLevel,
+                    skillType = prince.skillType,
+                    experienceYears = prince.experienceYears,
+                    princeId = prince.princeId,
+                    name = prince.name,
+                    age = prince.age
+                )
+            }
         }
     }
 }
--- src/test/java/com/makers/princemaker/service/PrinceMakerServiceTest.java
+++ src/test/java/com/makers/princemaker/service/PrinceMakerServiceTest.java
@@ -1,11 +1,15 @@
 package com.makers.princemaker.service;
 
-import com.makers.princemaker.dto.CreatePrince;
-import com.makers.princemaker.dto.PrinceDetailDto;
-import com.makers.princemaker.entity.Prince;
-import com.makers.princemaker.exception.PrinceMakerException;
-import com.makers.princemaker.repository.PrinceRepository;
-import com.makers.princemaker.code.PrinceMakerErrorCode;
+import static com.makers.princemaker.constant.PrinceMakerConstant.*;
+import static com.makers.princemaker.entity.PrinceMock.*;
+import static com.makers.princemaker.type.PrinceLevel.*;
+import static com.makers.princemaker.type.SkillType.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -13,120 +17,113 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
-
-import static com.makers.princemaker.constant.PrinceMakerConstant.MAX_JUNIOR_EXPERIENCE_YEARS;
-import static com.makers.princemaker.constant.PrinceMakerConstant.MIN_KING_EXPERIENCE_YEARS;
-import static com.makers.princemaker.entity.PrinceMock.createPrince;
-import static com.makers.princemaker.type.PrinceLevel.*;
-import static com.makers.princemaker.type.SkillType.INTELLECTUAL;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import com.makers.princemaker.code.PrinceMakerErrorCode;
+import com.makers.princemaker.dto.CreatePrince;
+import com.makers.princemaker.dto.PrinceDetailDto;
+import com.makers.princemaker.entity.Prince;
+import com.makers.princemaker.exception.PrinceMakerException;
+import com.makers.princemaker.repository.PrinceRepository;
 
 /**
  * @author Snow
  */
 @ExtendWith(MockitoExtension.class)
 class PrinceMakerServiceTest {
-    @Mock
-    private PrinceRepository princeRepository;
+	@Mock
+	private PrinceRepository princeRepository;
 
-    @InjectMocks
-    private PrinceMakerService princeMakerService;
+	@InjectMocks
+	private PrinceMakerService princeMakerService;
 
-    @Test
-    void getPrinceTest() {
-        //given
-        Prince juniorPrince =
-                createPrince(JUNIOR_PRINCE, INTELLECTUAL,
-                        MAX_JUNIOR_EXPERIENCE_YEARS, "princeId");
-        given(princeRepository.findByPrinceId(anyString()))
-                .willReturn(Optional.of(juniorPrince));
+	@Test
+	void getPrinceTest() {
+		//given
+		Prince juniorPrince =
+			createPrince(JUNIOR_PRINCE, INTELLECTUAL,
+				MAX_JUNIOR_EXPERIENCE_YEARS, "princeId");
+		given(princeRepository.findByPrinceId(anyString()))
+			.willReturn(Optional.of(juniorPrince));
 
-        //when
-        PrinceDetailDto prince = princeMakerService.getPrince("princeId");
+		//when
+		PrinceDetailDto prince = princeMakerService.getPrince("princeId");
 
-        //then
-        assertEquals(JUNIOR_PRINCE, prince.getPrinceLevel());
-        assertEquals(INTELLECTUAL, prince.getSkillType());
-        assertEquals(MAX_JUNIOR_EXPERIENCE_YEARS, prince.getExperienceYears());
-    }
+		//then
+		assertEquals(JUNIOR_PRINCE, prince.getPrinceLevel());
+		assertEquals(INTELLECTUAL, prince.getSkillType());
+		assertEquals(MAX_JUNIOR_EXPERIENCE_YEARS, prince.getExperienceYears());
+	}
 
-    @Test
-    void createPrinceTest_success() {
-        //given
-        CreatePrince.Request request = CreatePrince.Request.builder()
-                .princeLevel(MIDDLE_PRINCE)
-                .skillType(INTELLECTUAL)
-                .experienceYears(7)
-                .princeId("princeId")
-                .name("name")
-                .age(28)
-                .build();
-        ArgumentCaptor<Prince> captor =
-                ArgumentCaptor.forClass(Prince.class);
+	@Test
+	void createPrinceTest_success() {
+		//given
+		CreatePrince.Request request = new CreatePrince.Request(
+			MIDDLE_PRINCE,
+			INTELLECTUAL,
+			7,
+			"princeId",
+			"name",
+			28
+		);
 
-        //when
-        CreatePrince.Response response = princeMakerService.createPrince(request);
+		ArgumentCaptor<Prince> captor =
+			ArgumentCaptor.forClass(Prince.class);
 
-        //then
-        verify(princeRepository, times(1))
-                .save(captor.capture());
-        Prince savedPrince = captor.getValue();
-        assertEquals(MIDDLE_PRINCE, savedPrince.getPrinceLevel());
-        assertEquals(INTELLECTUAL, savedPrince.getSkillType());
-        assertEquals(7, savedPrince.getExperienceYears().intValue());
+		//when
+		CreatePrince.Response response = princeMakerService.createPrince(request);
 
-        assertEquals(MIDDLE_PRINCE, response.getPrinceLevel());
-        assertEquals(INTELLECTUAL, response.getSkillType());
-        assertEquals(7, response.getExperienceYears().intValue());
-    }
+		//then
+		verify(princeRepository, times(1))
+			.save(captor.capture());
+		Prince savedPrince = captor.getValue();
+		assertEquals(MIDDLE_PRINCE, savedPrince.getPrinceLevel());
+		assertEquals(INTELLECTUAL, savedPrince.getSkillType());
+		assertEquals(7, savedPrince.getExperienceYears().intValue());
 
-    @Test
-    void createPrinceTest_failed_with_duplicated() {
-        //given
-        Prince juniorPrince =
-                createPrince(JUNIOR_PRINCE, INTELLECTUAL, MAX_JUNIOR_EXPERIENCE_YEARS, "princeId");
-        CreatePrince.Request request = CreatePrince.Request.builder()
-                .princeLevel(JUNIOR_PRINCE)
-                .skillType(INTELLECTUAL)
-                .experienceYears(3)
-                .princeId("princeId")
-                .name("name")
-                .age(28)
-                .build();
-        given(princeRepository.findByPrinceId(anyString()))
-                .willReturn(Optional.of(juniorPrince));
+		assertEquals(MIDDLE_PRINCE, response.getPrinceLevel());
+		assertEquals(INTELLECTUAL, response.getSkillType());
+		assertEquals(7, response.getExperienceYears().intValue());
+	}
 
-        //when
-        PrinceMakerException exception =
-                assertThrows(PrinceMakerException.class, () -> princeMakerService.createPrince(request));
-        //then
-        assertEquals(PrinceMakerErrorCode.DUPLICATED_PRINCE_ID, exception.getPrinceMakerErrorCode());
-    }
+	@Test
+	void createPrinceTest_failed_with_duplicated() {
+		//given
+		Prince juniorPrince =
+			createPrince(JUNIOR_PRINCE, INTELLECTUAL, MAX_JUNIOR_EXPERIENCE_YEARS, "princeId");
+		CreatePrince.Request request = new CreatePrince.Request(
+			JUNIOR_PRINCE,
+			INTELLECTUAL,
+			3,
+			"princeId",
+			"name",
+			28);
+		given(princeRepository.findByPrinceId(anyString()))
+			.willReturn(Optional.of(juniorPrince));
 
-    @Test
-    void createPrinceTest_failed_with_invalid_experience() {
-        //given
-        CreatePrince.Request request = CreatePrince.Request.builder()
-                .princeLevel(KING)
-                .skillType(INTELLECTUAL)
-                .experienceYears(MIN_KING_EXPERIENCE_YEARS - 3)
-                .princeId("princeId")
-                .name("name")
-                .age(28)
-                .build();
-        given(princeRepository.findByPrinceId(anyString()))
-                .willReturn(Optional.empty());
+		//when
+		PrinceMakerException exception =
+			assertThrows(PrinceMakerException.class, () -> princeMakerService.createPrince(request));
+		//then
+		assertEquals(PrinceMakerErrorCode.DUPLICATED_PRINCE_ID, exception.getPrinceMakerErrorCode());
+	}
 
-        //when
-        PrinceMakerException exception =
-                assertThrows(PrinceMakerException.class, () -> princeMakerService.createPrince(request));
-        //then
-        assertEquals(PrinceMakerErrorCode.LEVEL_AND_EXPERIENCE_YEARS_NOT_MATCH, exception.getPrinceMakerErrorCode());
-    }
+	@Test
+	void createPrinceTest_failed_with_invalid_experience() {
+		//given
+		CreatePrince.Request request = new CreatePrince.Request(
+			KING,
+			INTELLECTUAL,
+			MIN_KING_EXPERIENCE_YEARS - 3,
+			"princeId",
+			"name",
+			28
+		);
+		given(princeRepository.findByPrinceId(anyString()))
+			.willReturn(Optional.empty());
+
+		//when
+		PrinceMakerException exception =
+			assertThrows(PrinceMakerException.class, () -> princeMakerService.createPrince(request));
+		//then
+		assertEquals(PrinceMakerErrorCode.LEVEL_AND_EXPERIENCE_YEARS_NOT_MATCH, exception.getPrinceMakerErrorCode());
+	}
 }
